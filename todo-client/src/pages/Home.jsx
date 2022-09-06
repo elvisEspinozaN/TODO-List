4044f86a71ea63ee426c1c818e72e0d25d8d0df9
elvisEspinoza <elvis_espinoza98@yahoo.com>
2022-09-05 21:40:51 -0400

    wip: table initialized

---
+++ todo-client/src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <table className="table table-hover table-dark">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Title</th>
+              <th scope="col">Tags</th>
+              <th scope="col">Created</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row"></th>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
